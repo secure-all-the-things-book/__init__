a6cabc8f1239b09269dcf607c2924a74c5bab347
Josh Long <joshua.long@broadcom.com>
2026-06-27 00:56:55 +0900

    up

---
+++ start.java
@@ -0,0 +1,41 @@
+void main() throws Exception {
+    var repositoryOrgName = "secure-all-the-things-book";
+
+    this.clone(repositoryOrgName);
+}
+
+private void clone(String organization) throws Exception {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor();) {
+        var start = Paths.get(".").resolve("../").toAbsolutePath().normalize().toString();
+        IO.println("initializing from " + start);
+        var uri = new URI("https://raw.githubusercontent.com/" +
+                organization + "/__init__/main/repositories.txt");
+        var url = uri.toURL();
+        var callables = new ArrayList<Callable<Void>>();
+        try (var reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            for (var line : reader.lines().toList()) {
+                var dirToCreate = new File(start, line);
+                var gitRepo = "git@github.com:" + organization + "/" + line + ".git";
+                callables.add(() -> {
+                    if (!dirToCreate.exists())
+                        runCommand(start, "git", "clone", gitRepo, dirToCreate.getAbsolutePath());
+                    else
+                        runCommand(dirToCreate.getAbsolutePath(), "git", "pull");
+                    return null;
+                });
+            }
+        }
+        executor.invokeAll(callables);
+    }
+}
+
+private void runCommand(String workingDir, String... command) throws Exception {
+    var pb = new ProcessBuilder(command);
+    pb.directory(new File(workingDir));
+    pb.inheritIO();
+    var process = pb.start();
+    var exitCode = process.waitFor();
+    if (exitCode != 0) {
+        IO.println("Command failed with exit code: " + exitCode);
+    }
+}
