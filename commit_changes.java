f488c318c4fc43895eaea80b542e3d5c2e1e6cee
Josh Long <joshua.long@broadcom.com>
2026-06-29 03:52:20 +0900

    automatic save @ 2026-06-28T18:52:20.918830Z

---
+++ commit_changes.java
@@ -0,0 +1,27 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25
+//DEPS org.springframework.boot:spring-boot-starter:4.1.0
+//DEPS org.asciidoctor:asciidoctorj:3.0.0
+
+void main() throws Exception {
+    var when = Instant.now() + "";
+    try (var br = new BufferedReader(new InputStreamReader(System.in), 1 << 16)) {
+        for (var line : br.readAllLines()) {
+            if (!line.isBlank()) {
+                var dir = Paths.get(line.trim());
+                if (Files.isDirectory(dir) && Files.isDirectory(dir.resolve(".git"))) {
+                    var c = run(dir, "git", "commit", "-am", "automatic save @ " + when);
+                    if (c == 0) {
+                        run(dir, "git", "push");
+                    } else {
+                        IO.println("commit returned " + c + " (nothing to commit?), skipping push");
+                    }
+                }
+            }
+        }
+    }
+}
+
+int run(Path dir, String... cmd) throws IOException, InterruptedException {
+    return new ProcessBuilder(cmd).directory(dir.toFile()).inheritIO().start().waitFor();
+}
