e6da0d6843c0a6470be92d9738e1e3ef6fb874cc
Josh Long <joshua.long@broadcom.com>
2026-06-30 06:34:20 +0900

    up

--- start.java
+++ start.java
@@ -4,17 +4,39 @@
 
 import org.springframework.core.io.UrlResource;
 
+private List<String> repositories(String propertiesBuffer) throws IOException {
+    var properties = new Properties();
+    var totalRepositories = new ArrayList<String>();
+    totalRepositories.add("__init__");
+    totalRepositories.add("design-assets");
+    try (var bin = new ByteArrayInputStream(propertiesBuffer.getBytes())) {
+        properties.load(bin);
+        if (properties.get("pipeline.job.code-repositories") instanceof String repositories) {
+            totalRepositories.addAll(Arrays
+                    .stream(repositories.split(",")) //
+                    .filter(l -> !l.isBlank()) //
+                    .map(uri -> {
+                        var arrImAPirate = uri.split("/");
+                        return arrImAPirate[arrImAPirate.length - 1]
+                                .replace(".git", "")
+                                .trim();
+                    }) //
+                    .toList());
+        }
+    }
+    return totalRepositories;
+}
+
 void main(String[] args) throws Exception {
     var organization = args.length > 0 ? args[0] : "secure-all-the-things-book";
     try (var executor = Executors.newVirtualThreadPerTaskExecutor();) {
         var start = Paths.get(".").toAbsolutePath().normalize().toString();
         IO.println("initializing from " + start);
         var uriResource = new UrlResource(new URI("https://raw.githubusercontent.com/" + organization +
-                "/__init__/refs/heads/main/repositories.txt?cb=" +System.currentTimeMillis()));
+                "/pipeline/refs/heads/main/src/main/resources/application.properties?cb=" + System.currentTimeMillis()));
         var callables = new ArrayList<Callable<Void>>();
-        var content =  uriResource.getContentAsString(Charset.defaultCharset());
-        IO.println("found " + content + " repositories");
-        for (var repoName : content.lines().toList())
+        var content =repositories( uriResource.getContentAsString(Charset.defaultCharset()));
+        for (var repoName : content)
             callables.add(buildCallable(start, organization, repoName));
         executor.invokeAll(callables);
     }
