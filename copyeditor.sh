ec2817bf72f4ea504c654cc74c52d751bb17a397
Josh Long <joshua.long@broadcom.com>
2026-08-03 00:38:13 -0400

    automatic save @ 2026-08-03T04:38:13.366378Z

--- copyeditor.sh
+++ copyeditor.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-claude "please fix grammar and spellchecking errors in the $1 file, preserving my asciidoctor formatting. i want this to read as though the New York Times grammar mavens scrutinized every character"
+
+
+claude "please fix grammar and spellchecking errors in the $1 file, preserving my asciidoctor formatting. i want this to read as though the New York Times grammar mavens scrutinized every character. after you are done, please identify any places where the content flows poorly. also, identify content that might be duplicative."
