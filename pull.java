02eff29683a963659c27ebf1a8274e3889ac117b
Josh Long <joshua.long@broadcom.com>
2026-08-06 08:59:41 -0700

    Add files via upload

---
+++ pull.java
@@ -0,0 +1,23 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25
+//DEPS org.springframework.boot:spring-boot-starter:4.1.0
+//SOURCES utils.java
+
+void main() throws IOException {
+    var start = Paths.get("").toAbsolutePath();
+    try (var stream = Files.walk(start)) {
+        stream.forEach(path -> {
+            if (Files.isDirectory(path) && path.getFileName().toString().equalsIgnoreCase(".git")) {
+                try {
+                    var proc = Runner.runAndReturnProcess(path, "git", "pull");
+                    try (var in = proc.getInputStream()) {
+                        if (!new String(in.readAllBytes()).isBlank())
+                            IO.println(path.getParent().toAbsolutePath().toString());
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
