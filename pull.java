bcc1bafbdecc11e14cf4e3a9981555cad76b85a2
Josh Long <joshua.long@broadcom.com>
2026-08-06 13:45:29 -0700

    automatic save @ 2026-08-06T20:45:29.511797Z

--- pull.java
+++ pull.java
@@ -3,14 +3,13 @@
 //DEPS org.springframework.boot:spring-boot-starter:4.1.0
 //SOURCES utils.java
 
-
 void main() throws IOException {
     try (var executor = Executors.newVirtualThreadPerTaskExecutor();) {
         var callables = new ArrayList<Callable<Void>>();
         var start = Paths.get("").toAbsolutePath();
         try (var stream = Files.walk(start)) {
             stream.forEach(path -> {
-                if (isDirectory(path))  callables.add(buildCallable(path));
+                if (isDirectory(path)) callables.add(buildCallable(path));
             });
         }
         executor.invokeAll(callables);
@@ -24,14 +23,14 @@ private boolean isDirectory(Path path) {
     return Files.isDirectory(path) && path.getFileName().toString().equalsIgnoreCase(".git");
 }
 
-
 private Callable<Void> buildCallable(Path path) {
     return () -> {
         try {
             var proc = Runner.runAndReturnProcess(path, "git", "pull");
             try (var in = proc.getInputStream()) {
-                if (!new String(in.readAllBytes()).isBlank())
-                    IO.println(path.getParent().toAbsolutePath().toString());
+                var output = new String(in.readAllBytes());
+                if (!output.isBlank() && !output.contains("Already up to date."))
+                    IO.println(path.getParent().toAbsolutePath().toString() + "\n\t" + output);
             }
         }// 
         catch (Exception e) {
